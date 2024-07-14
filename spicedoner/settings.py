7f30201f7ce9deda5966dccaeba3ffde34f035a1
Md. Asadozzaman <83003038+asadozzaman@users.noreply.github.com>
2024-07-14 12:28:30 +0600

    update

--- spicedoner/settings.py
+++ spicedoner/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static/']
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_URL = '/python/static/'
 STATIC_ROOT = '/usr/local/lsws/Example/html/demo/public/static'
 
 MEDIA_URL = '/media/'
